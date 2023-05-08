6493cc7cc22e50dfe7fdca822b2362e6696c275a
coolpythoncodes <coolpythoncodes@gmail.com>
2023-05-08 02:38:36 +0100

    working on creating campaign

--- src/modules/common/section/create-campaign/campaign-form.tsx
+++ src/modules/common/section/create-campaign/campaign-form.tsx
@@ -1,70 +1,95 @@
-/* eslint-disable @typescript-eslint/no-misused-promises */
-/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import Image, { type StaticImageData } from "next/image";
-import { Button, Form } from "antd";
+import { Button, DatePicker, Form, Select, Upload } from "antd";
 
 import back from "../../../../../public/assets/campaign/back.svg";
 import { TextArea, TextInput } from "../../component/inputs";
 import { generalRoutes } from "../../../../utils/data/routes.data";
 import Link from "next/link";
-// import { toast } from "react-hot-toast";
+import dayjs from "dayjs";
+import { UploadOutlined } from "@ant-design/icons";
 import {
-  // type CrowdFundContract,
-  // useContractContext,
+  type CrowdFundContract,
+  useContractContext,
 } from "~/context/ContractContext";
-// import { ethers } from "ethers";
+import { toast } from "react-hot-toast";
 
 const initialFormData = {
-  amount: "",
+  title: "",
+  goal: 0,
   description: "",
+  location: "",
+  category: "",
+  endAt: 0,
+  campaignImageUrl: "",
 };
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+const MAX_FILE_SIZE = 102400 // 100MB
+
 const CampaignForm = () => {
-  // const [creatingCampaign, setCreatingCampaign] = useState(false);
   const [form] = Form.useForm();
-  // const { initCrowdFundContractAddress } = useContractContext();
 
-  // const handleCreateCampaign = async () => {
-  //   const notification = toast.loading("Please wait...Don't leave this page");
-  //   setCreatingCampaign(true);
-  //   console.log(Math.floor(new Date().getTime() / 1000.0));
-  //   console.log(Math.floor(new Date().getTime() / 1000.0) + 86400)
+  const { initCrowdFundContractAddress } = useContractContext();
 
-  //   try {
-  //     const contract = initCrowdFundContractAddress() as CrowdFundContract;
+  const handleCreateCampaign = async () => {
+    const {
+      campaignImageUrl,
+      category,
+      description,
+      endAt,
+      goal,
+      location,
+      title,
+    } = (await form.validateFields()) as typeof initialFormData;
+    const notification = toast.loading("Creating Campaign.(Don't leave this page)");
+    try {
+      const contract = initCrowdFundContractAddress() as CrowdFundContract;
+      const txHash = await contract.createCampaign(
+        category,
+        goal,
+        description,
+        title,
+        // @ts-expect-error endAt type from antdesign datepicker
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access, @typescript-eslint/no-unsafe-call
+        Math.round(endAt.$d.getTime() / 1000),
+        location,
+        campaignImageUrl
+      )  as CrowdFundContract;
+      const receipt = await txHash.wait() ;
+      if (receipt) {
+        toast.success("Campaign was created successfully", {
+          id:notification
+        })
+      }
+    } catch (error) {
+      toast.error("Something went wrong", {
+        id: notification
+      })
+    }
+  };
 
-  //     const txHash = await contract.createCampaign(
-  //       "Education",
-  //       5000,
-  //       "School fees",
-  //       Math.floor(new Date().getTime() / 1000.0) + 1000,
-  //       Math.floor(new Date().getTime() / 1000.0) + 86400,
-  //       "Nigeria",
-  //       "https://ipfs.io/ipfs/QmTYEboq8raiBs7GTUg2yLXB3PMz6HuBNgNfSZBx5Msztg/robots.jpg"
-  //     );
-  //     const receipt = txHash.wait();
-  //     if (receipt) {
-  //       setCreatingCampaign(false);
-  //       toast.success("Campaign created successfully", {
-  //         id: notification,
-  //       });
-  //     }
-  //   } catch (error) {
-  //     console.log("error", error.message);
-  //     setCreatingCampaign(false);
-  //     toast.error("something went wrong", {
-  //       id: notification,
-  //     });
-  //   }
-  // };
-
-  // const handleSubmit = async (e: React.SyntheticEvent) => {
-  //   e.preventDefault();
-
-  //   const values = await form.validateFields();
-
-  //   console.log(values);
-  // };
+  const normFile = (e: unknown) => {
+    // @ts-expect-error e type unknown
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    if((e.file.size / 1024) < 100) {
+      console.log("error")
+      toast.error("File Size too large, minimum is 100mb")
+      return;
+    }
+    // @ts-expect-error e type unknown
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    console.log("Upload event:", e.file.size);
+    // @ts-expect-error e type unknown
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+    console.log(e.file.size / 1024)
+    if (Array.isArray(e)) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+      return e;
+    }
+    // @ts-expect-error e type unknown
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-return
+    return e?.fileList;
+  };
 
   return (
     <main className="bg-[#F5F5F5]">
@@ -86,8 +111,26 @@ const CampaignForm = () => {
             </p>
             <Form autoComplete="on" form={form}>
               <Form.Item
-                initialValue={initialFormData.amount}
-                name="amount"
+                initialValue={initialFormData.title}
+                name="title"
+                rules={[
+                  {
+                    required: true,
+                    message: "Title is required.",
+                  },
+                ]}
+              >
+                <TextInput
+                  label="Campaign Title"
+                  placeholder="Enter campaign title"
+                  id="title"
+                  name="title"
+                  type="string"
+                />
+              </Form.Item>
+              <Form.Item
+                initialValue={initialFormData.goal}
+                name="goal"
                 rules={[
                   {
                     required: true,
@@ -97,9 +140,9 @@ const CampaignForm = () => {
               >
                 <TextInput
                   label="How much would you like to raise?"
-                  placeholder="$0"
-                  id="amount"
-                  name="amount"
+                  placeholder="USDC 0"
+                  id="goal"
+                  name="goal"
                   type="number"
                 />
               </Form.Item>
@@ -120,13 +163,89 @@ const CampaignForm = () => {
                   name="description"
                 />
               </Form.Item>
+              <Form.Item
+                initialValue={initialFormData.location}
+                name="location"
+                rules={[
+                  {
+                    required: true,
+                    message: "Location is required.",
+                  },
+                ]}
+              >
+                <TextInput
+                  label="Location"
+                  placeholder="Enter the your location"
+                  id="location"
+                  name="location"
+                />
+              </Form.Item>
+              <Form.Item
+                initialValue={initialFormData.category}
+                name="category"
+                rules={[
+                  {
+                    required: true,
+                    message: "Category is required.",
+                  },
+                ]}
+                label="Category"
+              >
+                <Select>
+                  <Select.Option value="medical">Medical</Select.Option>
+                  <Select.Option value="emergency">Emergency</Select.Option>
+                  <Select.Option value="education">Education</Select.Option>
+                  <Select.Option value="nonprofit">Nonprofit</Select.Option>
+                  <Select.Option value="crisis relief">
+                    Crisis Relief
+                  </Select.Option>
+                </Select>
+              </Form.Item>
+              <Form.Item
+                label="Campaign ends?"
+                initialValue={initialFormData.endAt}
+                name="endAt"
+                rules={[
+                  {
+                    required: true,
+                    message: "Duration is required.",
+                  },
+                ]}
+              >
+                <DatePicker
+                  name="endAt"
+                  disabledDate={(current) => current < dayjs()}
+                />
+              </Form.Item>
+              <Form.Item
+                name="upload"
+                label="Upload"
+                valuePropName="fileList"
+                getValueFromEvent={normFile}
+              >
+                <Upload
+                  name="logo"
+                  action="/upload.do"
+                  listType="picture"
+                  accept=".png, .jpg"
+                  beforeUpload={(file) => {
+                    if((file.size / 1024) < 100) {
+                      console.log("error")
+                      toast.error("File Size too large, minimum is 100mb")
+                      return;
+                    }
+                  }}
+                >
+                  <Button icon={<UploadOutlined />}>Click to upload</Button>
+                </Upload>
+              </Form.Item>
               <div className="flex w-full justify-end">
                 <Button
                   // onClick={handleSubmit}
-                  // onClick={handleCreateCampaign}
-                  className="mt-4 h-[43px] w-[96px] border-none bg-[#FF6B00] text-base text-white md:mt-6"
+                  onClick={handleCreateCampaign as VoidFunction}
+                  className="mt-4 h-[43px] border-none bg-[#FF6B00] px-5 text-base text-white md:mt-6"
                 >
-                  Next
+                  Create campaign
                 </Button>
               </div>
             </Form>
